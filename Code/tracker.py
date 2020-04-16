5ba893f16fcebbbce120e464256449977714809c
Naman Gupta <namanryan@gmail.com>
2020-04-16 14:00:30 -0400

    Updated tracker.py
    Added difference between template and the image

--- Code/tracker.py
+++ Code/tracker.py
@@ -1,12 +1,46 @@
 import cv2
 import glob
+import numpy as np
 
+
+# function to compute new warping parameters
+def affineLKtracker(image, tmp, rect):
+
+    # get perspective matrix
+    M = cv2.getPerspectiveTransform(rect, dst)
+
+    # get warped image
+    W = cv2.warpPerspective(image, M, (122, 89))
+
+    # difference of template and warped image
+    diff = cv2.subtract(tmp, W)
+
+    # display warped image
+    cv2.imshow('warped image', W)
+    # print(W.shape)
+    cv2.imshow('difference', diff)
+
+
+# main function
 if __name__ == '__main__':
 
-    filenames = glob.glob("Dataset/DragonBaby/DragonBaby/img/*.jpg")
+    # get images from folder
+    filenames = glob.glob("Dataset/Car4/img/*.jpg")
     filenames.sort()
-    images = [cv2.imread(img) for img in filenames]
+    images = [cv2.imread(img, 0) for img in filenames]
+
+    # get template from folder
+    template = cv2.imread('Dataset/templates/croppedCar.png', 0)
+    # print(template.shape)
+
+    # bounding box coordinates in image
+    box = np.array([[67, 51], [189, 52], [189, 139], [67, 138]], dtype='float32')
+
+    # destination point coordinates for warping
+    dst = np.array([[0, 0], [122, 0], [122, 89], [0, 89]], dtype='float32')
 
     for img in images:
-        cv2.imshow('image', img)
+
+        affineLKtracker(img, template, box)
+        # cv2.imshow('image', img)
         cv2.waitKey(0)
